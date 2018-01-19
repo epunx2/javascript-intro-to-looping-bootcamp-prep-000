9eb543282866689e0b7db429ade4c8a961374578
Eric Chrobak <eric@ericchrobak.com>
2018-01-19 03:35:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 var forLoop = function(array){
   for(var i = 0; i < 25; i++){
-    console.log("I am $(i) strange loop${i === 0 ? '' : 's'}.");
+    array.push("I am $(i) strange loop${i === 0 ? '' : 's'}.");
   }
+  return array;
 }
